6e50d4f7dcfa409c70c18d3eb4513763f16c501a
Daniel <dhahaj@gmail.com>
2025-06-21 03:21:28 -0500

    refactor: add type hints to parse and convert functions

--- ini/__init__.py
+++ ini/__init__.py
@@ -1,36 +1,36 @@
-def parse(text):
+def parse(text: str) -> dict:
     data = {}
-    section = ''
+    section = ""
 
-    for line in text.split('\n'):
-        if line.startswith(';'):
+    for line in text.split("\n"):
+        if line.startswith(";"):
             continue
 
-        elif line.startswith('[') and line.endswith(']'):
-            line = line.replace('[', '')
-            line = line.replace(']', '')
-            
+        elif line.startswith("[") and line.endswith("]"):
+            line = line.replace("[", "")
+            line = line.replace("]", "")
+
             section = line.strip()
-            
+
             if not data.get(section):
                 data[section] = {}
+        
+        elif "=" in line:
+            key, value = line.split("=")[0], "=".join(line.split("=")[1:])
+            key = key.replace('"', "")
+            key = key.replace("'", "")
+            key = key.strip()
 
-        else:
-            if "=" in line:
-                key, value = line.split("=")[0], "=".join(line.split("=")[1:])              
-                key = key.replace("\"", "")
-                key = key.replace("\'", "")
-                key = key.strip()
-                
-                value = value.replace("\"", "")
-                value = value.replace("\'", "")
-                value = value.strip()
+            value = value.replace('"', "")
+            value = value.replace("'", "")
+            value = value.strip()
 
-                data[section][key] = value
+            data[section][key] = value
 
     return data
 
-def convert(data):
+
+def convert(data: dict) -> str:
     text = ""
     for section in data:
         text += "[%s]\n" % section
@@ -41,3 +41,15 @@ def convert(data):
         text += "\n"
 
     return text.strip()
+
+
+def load(file: str) -> dict:
+    with open(file, "r") as f:
+        text = f.read()
+    return parse(text)
+
+def dump(data: dict, file: str) -> str:
+    with open(file, "w") as f:
+        text = convert(data)
+        f.write(text)
+    return text
