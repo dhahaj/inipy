8e1e7511b6a0314748b3320aaca369afc25b384f
Daniel <dhahaj@gmail.com>
2025-06-21 03:21:22 -0500

    update repository URL in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="a lightweight ini parser",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/husseinraed/ini.py",
+    url="https://github.com/dhahaj/inipy",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
